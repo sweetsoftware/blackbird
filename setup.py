5fc2de50e847b8fff3cd94dd6641810424596355
sweetsoftware <sweetsoftware@localhost.localdomain>
2019-06-19 11:18:02 +0200

    Fixes #1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 import os
 
+
+os.system('pip3 install -r requirements.txt')
+
 from blackbird import utils
-
-
 for module in utils.get_module_list():
     os.system('./blackbird/modules/%s/setup.sh' % module)
 
-os.system('pip3 install -r requirements.txt')
