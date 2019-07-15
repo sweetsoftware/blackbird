415b7e9c7d881730d570760aa28bc5ef7f42271f
sweetsoftware <sweetsoftware@localhost>
2019-07-15 12:20:00 +0200

    Adds interactive menu and improves recon scan pool
    Fast scan is performed by default
    Bug fix in nmap parsing
    Renamed output dir to working dir

--- blackbird/config.py
+++ blackbird/config.py
@@ -3,7 +3,7 @@ import os
 CUSTOM_USER_LIST = ""
 CUSTOM_PASS_LIST = ""
 CUSTOM_USERPASS_LIST = ""
-FAST_SCAN = False
+FULL_SCAN = False
 BRUTE = False
 ENUM = False
 SCAN = False
@@ -13,3 +13,4 @@ SWEEP = False
 MODULES = []
 ONLY_CUSTOM_BRUTE = False
 INSTALL_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+MAX_JOBS = 10
